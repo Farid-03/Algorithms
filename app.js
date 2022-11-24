8b58e04967a86d0ba5aa216939ef4e33c860539b
Farid <100301265+Farid-03@users.noreply.github.com>
2022-11-25 01:42:58 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// Task1
+
+
+/* var n;
+
+function Dec(number) {
+    for (var i = n - 1; i > n; i--){
+        if (i % 7 == 0) {
+            console.log(i);
+        }
+    }
+}
+
+if (n % 7 == 0) {
+    console.log("7`ə bölünür");
+} else {
+    Dec(number);
+}
+ */
+
+//Task 2
+
+/* var n;
+
+function Dec(n) {
+    var c = 0;
+    for (var i = 3; i < n; i += 3) {
+        c += 1;
+    }
+    console.log(c);
+}
+
+if (n < 50) {
+    Dec(n);
+} */
+
+//Task 3
+
+/* var n;
+
+function Dec(n) {
+    var c = 0;
+    for (var i = 5; i < n; i += 3) {
+        c += 1;
+    }
+    console.log(c);
+}
+
+if (n > 50 && n < 100){
+    Dec(n);
+} */
+
+/* Task 4 */
+
+/* var n;
+
+function Dec(n) {
+    var c = 0;
+    for (var i = 8; i < n; i += 8) {
+        c += 1;
+    }
+    console.log(c);
+}
+
+if (n > 100) {
+    Dec(n);
+} */
